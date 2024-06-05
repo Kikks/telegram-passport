a6278b6b485bf4bff2acd988b0cfdd5b11abafbc
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2024-06-05 21:28:31 +0100

    🔥 Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import "./App.css";
 
 import TelegramLoginButton, { TelegramUser } from "telegram-login-button";
+// @ts-ignore
 import TelegramLoginButton2 from "react-telegram-login";
 
 function App() {
@@ -11,7 +14,7 @@ function App() {
 				dataOnauth={(user: TelegramUser) => console.log(user)}
 			/>
 			<TelegramLoginButton2
-				dataOnauth={user => console.log(user)}
+				dataOnauth={(user: any) => console.log(user)}
 				botName='tpassport_test_bot'
 			/>
 		</>
